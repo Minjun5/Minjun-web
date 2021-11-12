a8e49cc6901f70f1c713252975528b6a355dd378
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2021-11-12 19:08:13 +0900

    Update Homework2.js

--- Hoemwork2/Homework2.js
+++ Hoemwork2/Homework2.js
@@ -1,6 +1,8 @@
 //output=68degrees
 let kelvin=293
+//output=celsius
 let celsius=kelvin-273
+//output=fahrenheit
 let fahrenheit=celsius*(9/5)+32
 console.log(fahrenheit)
 console.log('The temperature is '+fahrenheit +'degrees Fahrenheit.')

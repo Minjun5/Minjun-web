1605ae0cda1f53a7b097235662a33aa9f4394635
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2021-12-15 09:13:48 +0900

    Add files via upload

---
+++ Homework5/Homework5.js
@@ -0,0 +1,41 @@
+let secretMessage = [
+    "Learning",
+    "is",
+    "not",
+    "about",
+    "what",
+    "you",
+    "get",
+    "easily",
+    "the",
+    "first",
+    "time,",
+    "it",
+    "is",
+    "about",
+    "what",
+    "you",
+    "can",
+    "figure",
+    "out.",
+    "-2015,",
+    "Chris",
+    "Pine,",
+    "Learn",
+    "JavaScript",
+  ]
+console.log(secretMessage.length)
+secretMessage.push('to','program')
+  console.log(secretMessage)
+secretMessage[7]='right'
+console.log(secretMessage)
+secretMessage.unshift('programming','or','coding')
+console.log(secretMessage)
+const part1= secretMessage.slice(24)
+console.log(part1)
+console.log(part1.length)
+function part2(A)
+{if(A.length>2){return 'UPDATED'}
+else {return 'undefined'}}
+console.log(part2(secretMessage))
+console.log(part2(part1))
